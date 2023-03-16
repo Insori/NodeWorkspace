21c8e50a1bcddd78e7e4c73d69176db7d856fdde
SoRi In <s2110@e-mirim.hs.kr>
2023-03-16 19:00:08 +0900

    process 예제

---
+++ ch02_test01.js
@@ -0,0 +1,5 @@
+console.log("argv 속성의 파라미터 수 : " + process.argv.length);
+console.dir(process.argv);
+
+console.dir(process.env);
+//console.log("os 환경변수의 값 : "+process.env[OS]);   //접근할 수 없다는 오류 발생
