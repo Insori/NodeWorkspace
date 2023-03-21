951f12e74346cd9d6ce9cfaa0b170462690bd673
SoRi In <s2110@e-mirim.hs.kr>
2023-03-21 19:00:23 +0900

    백틱(`)

---
+++ ch03_test01.js
@@ -0,0 +1,11 @@
+let pi = 3.14159265;
+let radius = 10;    //반지름
+
+console.log("원의 둘레 : "+(2*pi*radius));
+console.log("원의 넓이 : "+(pi*radius*radius)+"\n");
+
+console.log(`원의 둘레 : ${2*pi*radius}`);
+console.log(`원의 넓이 : ${pi*radius*pi}\n`);
+
+console.log("반지름 : "+radius+"\n");
+console.log(`반지름 : ${radius}`);
