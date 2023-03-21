5ec511cdf036f0202e8625588a95f941dd55182c
SoRi In <s2110@e-mirim.hs.kr>
2023-03-21 19:29:03 +0900

    switch 조건문

--- ch03_test05.js
+++ ch03_test05.js
@@ -14,3 +14,26 @@ if(hours < 11) {
         console.log("저녁 먹을 시간입니다.");
     }
 }
+
+switch(date.getMonth()+1) {
+    case 12:
+    case 1:
+    case 2:
+        console.log("겨울입니다.");
+        break;
+    case 3:
+    case 4:
+    case 5:
+        console.log("봄입니다.");
+        break;
+    case 6:
+    case 7:
+    case 8:
+        console.log("여름입니다.");
+        break;
+    case 9:
+    case 10:
+    case 11:
+        console.log("가을입니다.");
+        break;
+}
