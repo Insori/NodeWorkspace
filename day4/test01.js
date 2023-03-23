d194b3ce4159bf98a167200c1f929840f9e481b8
SoRi In <s2110@e-mirim.hs.kr>
2023-03-23 18:51:51 +0900

    for in, for of반복문

---
+++ day4/test01.js
@@ -0,0 +1,18 @@
+let array = ["사과", "배", "포도", "딸기", "바나나"];
+
+for(let i in array) {
+    console.log(`${i}번째 요소: ${array[i]}`);
+}
+
+console.log('-----------------------');
+
+for(let item of array) {
+    console.log(item);
+}
+
+console.log('-----------------------');
+
+//보통의 for문
+for(let i=0; i<array.length; i++) {
+    console.log(array[i]);
+}
