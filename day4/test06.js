428eb612f1887f0859e6dbbf4c6cb5d997c3071e
SoRi In <s2110@e-mirim.hs.kr>
2023-03-23 19:10:24 +0900

    매개변수가 여러 개인 함수, 리턴 없는 함수

---
+++ day4/test06.js
@@ -0,0 +1,15 @@
+function multiple(x, y) {
+    return x * y;
+}
+
+console.log(multiple(32, 27));
+console.log(multiple(83, 32));
+
+console.log('---------');
+
+function print(message) {
+    console.log(`${message}(이)라고 말했습니다.`);
+}
+
+print("자바스크립트");
+print("별 거 아니네");
