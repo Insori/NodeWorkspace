fd04a992e4aa487094d986409178fade0dbe9bdd
Insori <s2110@e-mirim.hs.kr>
2023-05-18 19:39:50 +0900

    todo list

--- todolist/app.js
+++ todolist/app.js
@@ -2,41 +2,81 @@ let express = require('express');
 let ejs = require('ejs');
 let path = require('path');
 
-let app = express();
-
 let todoArr = [
-    {id : 1, contents : '무한 열차 보기', yesno : 'no'},
-    {id : 2, contents : '배드민턴 하기', yesno : 'no'},
-    {id : 3, contents : '샌드위치 맛집 찾기', yesno : 'no'},
-    {id : 3, contents : '캐나다로 여행 가기', yesno : 'no'}
+    { id: 1, contents: '영화 보기', yesno: 'no' },
+    { id: 2, contents: '숙제 하기', yesno: 'no' },
+    { id: 3, contents: '운동 하기', yesno: 'no' },
+    { id: 4, contents: '노래 듣기', yesno: 'no' }
 ];
 
-app.use(express.static('public'));  //public 폴더 공유
-app.use(express.urlencoded({extended:false}));  //사용자 html 입력
-app.set('views', path.join(__dirname, 'views'));   //뷰 폴더 지정
+let count = 5;
+
+let app = express();
+
+app.use(express.static('public'));  // pulbic 폴더 공유
+app.use(express.urlencoded({ extended: false }));  // 사용자 html 입력
+app.set('views', path.join(__dirname, 'views'));    // 뷰 폴더
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    console.log('/ get이 시작됨');
-    res.render('list', {datalist: todoArr});    //list.ejs 화면에 출력
+    console.log('/ get이 시작됨~~');
+    res.render('list', { datalist: todoArr });   // list.ejs 화면에 출력
 });
 
 app.get('/insert', (req, res) => {
-    console.log("/insert get 시작됨");
-    //추가
+    console.log("/insert get 시작됨~");
+    res.render('insert');
 });
 
 app.post('/insert', (req, res) => {
-    console.log("/insert get 시작됨");
-    //추가
-});
-
-app.get('/delete:id', (req, res) => {
-    console.log("/delete" + id);
-    //추가
+    console.log("/insert post 시작됨~");
+    //배열에 입력 받은 값으로 객체를 만들어 추가
+    let id_num = count++;
+    todoArr.push({id:id_num, contents:req.body.contents, yesno: req.body.yesno});
     res.redirect("/");
 });
 
+app.get('/delete/:id', (req, res) => {
+    console.log("/delete " + res.id);
+    for(const i in todoArr) {
+        if(todoArr[i].id == req.params.id) {
+            console.log(todoArr[i].id + " " + i);
+            todoArr.splice(i, 1);
+        }
+    }
+    console.log("delete ok~~~ " + todoArr);
+    res.redirect("/");
+});
+
+app.get('/edit/:id', (req, res) => {
+    let editdata = [];
+    console.log("/edit " + req.params.id);
+    for(const i in todoArr) {
+        if(todoArr[i].id == req.params.id) {
+            console.log(todoArr[i].id + " " + i);
+            editdata = todoArr[i];
+            res.render('edit', {data: editdata}); //edit.ejs
+        }
+    }
+})
+
+app.post('/edit/:id',(req, res) => {
+    let editdata = [];
+    console.log("/edit " + req.params.id);
+    if(req.body.contents && req.body.yesno) {
+        console.log("수정 값 " + req.body.contents + " " + req.body.yesno);
+    }
+
+    //추가
+    for (const i in todoArr) {
+        if(todoArr[i].id == req.params.id) {
+            todoArr.splice(i, 1, {id:req.params.id, contents:req.body.contents, yesno:req.body.yesno});
+            console.log("수정 ok" + req.params.id);
+        }
+    }
+    res.redirect("/");
+})
+
 app.listen(8000, () => {
-    console.log('8000 포트에서 서버 시작');
+    console.log("8000 포트 서버가 시작됨");
 });
